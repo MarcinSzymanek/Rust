c75519b498970fb9d5dc63ef8d6ce44d760c4d04
Marcin Szymanek <nastrunach@gmail.com>
2024-10-10 14:54:41 +0200

    Cleaned imports and unused vars. Marked os vars as unused. Now exits gracefully instead of panic on invalid arg. Display whether contents of directory are a file, directory or a simlink

--- rust-ls/src/main.rs
+++ rust-ls/src/main.rs
@@ -1,8 +1,9 @@
+use std::fs::ReadDir;
 use std::{env, fs};
 use std::env::consts::OS;
 
 use clap::Parser;
-use colored::{ColoredString, Colorize};
+use colored::Colorize;
 use log::{trace, debug, info, warn};
 
 mod os_types;
@@ -24,7 +25,6 @@ struct Args {
 fn main() {
     let args = Args::parse();
     let os_args: Vec<_> = env::args().collect();
-    let target: String;
 
     // Get target arg and ignore all clap args
     // Turns out this is unnecessary: clap does that by default?
@@ -35,7 +35,7 @@ fn main() {
 
             // If first 2 characters are "--" assume clap args
             // This never prints, so clap::Parser does it by default?
-            if (first == '-' && second == '-'){
+            if first == '-' && second == '-'{
                 debug!("Ignored arg: {}, assumed clap Arg", val);
                 continue;
             }
@@ -45,27 +45,33 @@ fn main() {
     }
     warn!("Going to dir: {}", args.dir);
 
-    let dir_contents = fs::read_dir(args.dir).expect("Could not read directory");
+    let dir_contents: ReadDir;
+    match fs::read_dir(args.dir.as_str()){
+        Ok(contents) => dir_contents = contents,
+        Err(error) => {
+            println!("Could not go to dir '{}'\nerror: '{}'", args.dir, error);
+            return;
+        }
+    }
 
     // Setup based on OS right now unused
-    let use_filetypes: bool;
-    let use_simlink: bool;
+    let _use_filetypes: bool;
+    let _use_simlink: bool;
 
+    trace!("OS is: {}", OS);
     match OS{
         os_types::OsTypes::WINDOWS => {
-            println!("OS is: {}", os_types::OsTypes::WINDOWS);
-            use_filetypes = true;
-            use_simlink = false;
+            _use_filetypes = true;
+            _use_simlink = false;
         },
         os_types::OsTypes::LINUX => {
-            println!("OS is not {}", os_types::OsTypes::WINDOWS);
-            use_filetypes = false;
-            use_simlink = true;
+            _use_filetypes = false;
+            _use_simlink = true;
         },
         _ => {
-            println!("Could not detect OS, got: {}", OS);
-            use_filetypes = false;
-            use_simlink = false;
+            trace!("Could not detect OS, got: {}", OS);
+            _use_filetypes = false;
+            _use_simlink = false;
         }
     }
 
@@ -77,15 +83,15 @@ fn main() {
         spaces-=1;
     }
     output.push_str(&whitespaces);
-    output.push_str("type");
+    output.push_str("type\n");
     println!("{}", output);
 
     for val in dir_contents{
         let value = val.as_ref().unwrap();
-        // if !value.unwrap().file_type().unwrap().is_dir(){
-        //     continue;
-        // }
-        let color =  if value.file_type().unwrap().is_dir() {colored::Color::Green} else {colored::Color::White};
+        let is_dir: bool = value.file_type().unwrap().is_dir();
+        let is_link: bool = value.file_type().unwrap().is_symlink();
+
+        let color =  if is_dir {colored::Color::Green} else {colored::Color::White};
 
         let filename = value.file_name().into_string().unwrap();
         let mut spaces = 40 - filename.chars().count();
@@ -94,10 +100,18 @@ fn main() {
             whitespaces.push_str(" ");
             spaces-=1;
         }
-        let output: ColoredString = colored::ColoredString::try_from(value.file_name().into_string().unwrap()).expect("Could not color string");
+
+        let mut output: String = filename;
+        output.push_str(&whitespaces);
+        if is_dir{
+            output.push_str("dir\n");
+        } else if is_link{
+            output.push_str("link\n");
+        } else{
+            output.push_str("file\n");
+        }
 
         print!("{}", output.color(color));
-        print!("{}\n", whitespaces);
     }
 
 }
